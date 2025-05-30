36c59a8d9af80d88c67cb129f3332b25c537776e
Psalm Muelvin Lopez <muelvinlopez25@gmail.com>
2025-05-30 13:17:44 +0800

    complete and exit

--- to-do_app/gui.py
+++ to-do_app/gui.py
@@ -10,11 +10,15 @@ list_box = FGS.Listbox(values=functions.get_todos(),
                        key="todos",
                        enable_events=True,
                        size=[45, 10])
+
 edit_button = FGS.Button("Edit")
+complete_button = FGS.Button("Complete")
+Exit_button = FGS.Button("Exit")
 
 window = FGS.Window("Muelvin's Today Lists", layout=[
                     [label1, input_box, add_button],
-                    [list_box, edit_button]],
+                    [list_box, edit_button, complete_button],
+                    [Exit_button]],
                     font=("Arial", 15))
 
 while True:
@@ -30,6 +34,7 @@ while True:
             functions.write_todos(todos)
 
             window['todos'].update(values=todos)
+            window['todo'].update(value='')
 
         case "Edit":
             todo_to_edit = value['todos'][0]
@@ -42,8 +47,25 @@ while True:
 
             window['todos'].update(values=todos)
 
+        case "Complete":
+            todo_to_complete = value['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+
+            # This is my personal code
+            # index = todos.index(todo_to_complete)
+            # todos.pop(index)
+            # functions.write_todos(todos)
+
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+
         case "todos":
             window['todo'].update(value=value['todos'][0])
+
+        case "Exit":
+            break
 
         case FGS.WIN_CLOSED:
             break
